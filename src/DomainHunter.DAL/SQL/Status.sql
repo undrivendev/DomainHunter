ecccfe5b5ca21847090ae82f499b07d8fd940e6d
Luca Dalla Valle <lucadv91@gmail.com>
2019-01-26 14:48:18 +0100

    added status

---
+++ src/DomainHunter.DAL/SQL/Status.sql
@@ -0,0 +1,6 @@
+﻿select 'free' kind, count(*) from "domain" where status = 0
+union
+select 'taken' kind, count(*) from "domain" where status = 1
+union
+select 'errors' kind, count(*) from "domain" where status = 2
+order by 1
